f57ee5cec5ac1d3af49c3dbb492a699df71cc2a7
Fritz Otlinghaus <fritz@otlinghaus.it>
2015-09-26 13:25:50 +0200

    moved to postgresql

--- private_warehouse_django/private_warehouse_django/urls.py
+++ private_warehouse_django/private_warehouse_django/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from product.views import ProductViewSet
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+router = routers.SimpleRouter()
+router.register(r'products', ProductViewSet)
+urlpatterns = router.urls
+
+
--- private_warehouse_django/product/models.py
+++ private_warehouse_django/product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    #category = models.ForeignKey('Category')
+    #manufacturer = models.ForeignKey('Manufacturer')
+    tags = TaggableManager()
+    #supplier = models.ForeignKey('Supplier')
+    #image_url = models.ImageField()
+    expiration_date = models.DateTimeField()
+    storage_location = models.TextField()
+
