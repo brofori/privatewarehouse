3f0b368902b3f1157b8c6826af4504b375393adb
Fritz Otlinghaus <fritz@otlinghaus.it>
2015-09-27 06:55:29 +0200

    sutfasd

--- private_warehouse_django/product/views.py
+++ private_warehouse_django/product/views.py
@@ -16,7 +16,6 @@ class ItemViewSet(ModelViewSet):
         """
         queryset = Item.objects.all()
         barcode = self.request.query_params.get('barcode', None)
-        print(barcode)
         if barcode is not None:
             queryset = queryset.filter(barcode=barcode)
         return queryset
@@ -27,6 +26,16 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Product.objects.all()
+        item_id = self.request.query_params.get('item_id', None)
+        if item_id is not None:
+            queryset = queryset.filter(item__id=item_id)
+        return queryset
 
 class PriceViewSet(ModelViewSet):
     queryset = Price.objects.all()
