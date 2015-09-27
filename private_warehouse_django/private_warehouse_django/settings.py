17f94dada4966907cc6efad05babcefbbd01fb74
Fritz Otlinghaus <fritz@otlinghaus.it>
2015-09-27 05:23:03 +0200

    added cors

--- private_warehouse_django/private_warehouse_django/settings.py
+++ private_warehouse_django/private_warehouse_django/settings.py
@@ -44,13 +44,15 @@ INSTALLED_APPS = (
     'product',
     'supplier',
     'manufacturer',
-    'household'
+    'household',
+    'corsheaders',
 )
 
 SHELL_PLUS = 'ipython'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,7 +61,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'private_warehouse_django.urls'
 
 TEMPLATES = [
