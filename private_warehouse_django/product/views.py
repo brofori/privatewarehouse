ed96ddb27f8706b18876eb52466b9e3fb23d5c6b
Fritz Otlinghaus <fritz@otlinghaus.it>
2015-09-27 05:13:10 +0200

    added barcode queryparam

--- private_warehouse_django/product/views.py
+++ private_warehouse_django/product/views.py
@@ -6,8 +6,20 @@ from .serializers import ItemSerializer, ProductSerializer, PriceSerializer
 
 
 class ItemViewSet(ModelViewSet):
-    queryset = Item.objects.all()
+
     serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Item.objects.all()
+        barcode = self.request.query_params.get('barcode', None)
+        if barcode is not None:
+            queryset = queryset.filter(barcode=barcode)
+        return queryset
+
 
 
 class ProductViewSet(ModelViewSet):
