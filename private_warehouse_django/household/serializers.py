d8a0221196987539d40435221d8bf9e8c601bfc4
Fritz Otlinghaus <fritz@otlinghaus.it>
2015-09-27 07:46:04 +0200

    fields for parsing

--- private_warehouse_django/household/serializers.py
+++ private_warehouse_django/household/serializers.py
@@ -1,6 +1,7 @@
 __author__ = 'fritz'
 from .models import Household, HouseholdProductMap, HouseholdItemMap
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 
 class HouseholdSerializer(ModelSerializer):
@@ -9,5 +10,9 @@ class HouseholdSerializer(ModelSerializer):
 
 
 class HouseholdProductMapSerializer(ModelSerializer):
+    name = serializers.CharField(source="product.item.name")
+    image_url = serializers.URLField(source="product.item.image_url")
+    product_id = serializers.CharField(source="product.id")
+    
     class Meta:
         model = HouseholdProductMap
